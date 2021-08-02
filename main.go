57a04be6672eb14fec4e495302d04c2b757d4262
KSlashh <48985735+KSlashh@users.noreply.github.com>
2021-08-02 17:11:23 +0800

    update

--- main.go
+++ main.go
@@ -39,11 +39,7 @@ func init() {
 		"  -func restartCCM -mul {1} -conf {./config.json} [ChainID-1] [ChainID-2] ... [ChainID-n] \n"+
 		"  -func shutToken -mul {1} -conf {./config.json} -token {./token.json} \n"+
 		"  -func rebindToken -mul {1} -conf {./config.json} -token {./token.json} \n"+
-<<<<<<< Updated upstream
-		"  ## {} contains default value")
-=======
 		"  {}contains default value")
->>>>>>> Stashed changes
 	flag.Parse()
 }
 
@@ -77,10 +73,7 @@ func main() {
 				log.Errorf("fail to dial client %s of network %d", netCfg.Provider, id)
 			}
 			go func() {
-<<<<<<< Updated upstream
-=======
 				log.Infof("Shutting down %s ...", netCfg.Name)
->>>>>>> Stashed changes
 				err = shutTools.ShutCCM(multiple, client, netCfg)
 				sig <- Msg{netCfg.PolyChainID, err}
 			}()
@@ -122,10 +115,7 @@ func main() {
 				log.Errorf("fail to dial client %s of network %d", netCfg.Provider, id)
 			}
 			go func() {
-<<<<<<< Updated upstream
-=======
 				log.Infof("Restarting %s ...", netCfg.Name)
->>>>>>> Stashed changes
 				err = shutTools.RestartCCM(multiple, client, netCfg)
 				sig <- Msg{netCfg.PolyChainID, err}
 			}()
@@ -280,7 +270,6 @@ func main() {
 						return
 					}
 					log.Infof("%s: %d =>to=> %d pair has be rebind", tokenConfig.Name, tokens[i].ChainId, tokens[j].ChainId)
-<<<<<<< Updated upstream
 				}
 				sig <- Msg{tokens[i].ChainId, nil}
 			}(i)
@@ -298,167 +287,6 @@ func main() {
 				break
 			}
 		}
-=======
-				}
-				sig <- Msg{tokens[i].ChainId, nil}
-			}(i)
-		}
-		cnt := len(tokens)
-		for msg := range sig {
-			cnt -= 1
-			if msg.Err != nil {
-				log.Error(msg.Err)
-			} else {
-				log.Infof("%s at chain %d has been rebind.", tokenConfig.Name, msg.ChainId)
-			}
-			if cnt == 0 {
-				log.Info("Done.")
-				break
-			}
-		}
-		/*
-			case "shutToken":
-				log.Info("Processing...")
-				args := flag.Args()
-				sig := make(chan Msg, 10)
-				if len(args)%2 != 0 {
-					log.Fatalf("invalid arg amount %d ,must be even", len(args))
-				}
-				var tokens []*token
-				for i := 0; i < len(args); i += 2 {
-					id, err := strconv.Atoi(args[i])
-					if err != nil {
-						log.Fatalf("can not parse arg %d : %s , %v", i, args[i], err)
-					}
-					address := args[i+1]
-					if !common.IsHexAddress(address) {
-						log.Fatalf("%s is not a valid address", address)
-					}
-					netCfg := conf.GetNetwork(uint64(id))
-					if netCfg == nil {
-						log.Fatalf("network with chainId %d not found in config file", id)
-					}
-					tokens = append(tokens, &token{uint64(id), address, netCfg})
-				}
-				for i := 0; i < len(tokens); i++ {
-					go func(i int) {
-						log.Infof("Shutting down token for %s...",tokens[i].NetCfg.Name)
-						client, err := ethclient.Dial(tokens[i].NetCfg.Provider)
-						if err != nil {
-							err = fmt.Errorf("fail to dial %s , %s", tokens[i].NetCfg.Provider, err)
-							sig <- Msg{tokens[i].ChainId, err}
-							return
-						}
-						for j := 0; j < len(tokens); j++ {
-							if i == j {
-								continue
-							}
-							err = shutTools.BindToken(
-								multiple,
-								client,
-								tokens[i].NetCfg,
-								common.HexToAddress(tokens[i].Address),
-								tokens[j].ChainId,
-								nil)
-							if err != nil {
-								err = fmt.Errorf(
-									"fail to shut bind from chain %d =>to=> chain %d , %s",
-									tokens[i].ChainId,
-									tokens[j].ChainId,
-									err)
-								sig <- Msg{tokens[i].ChainId, err}
-								return
-							}
-							log.Infof("%d =>to=> %d pair has be unbind",tokens[i].ChainId,tokens[j].ChainId)
-						}
-						sig <- Msg{tokens[i].ChainId, nil}
-					}(i)
-				}
-				cnt := len(tokens)
-				for msg := range sig {
-					cnt -= 1
-					if msg.Err != nil {
-						log.Error(msg.Err)
-					} else {
-						log.Infof("Token at chain %d has been shut down.", msg.ChainId)
-					}
-					if cnt == 0 {
-						log.Info("Done.")
-						break
-					}
-				}
-			case "rebindToken":
-				log.Info("Processing...")
-				args := flag.Args()
-				sig := make(chan Msg, 10)
-				if len(args)%2 != 0 {
-					log.Fatalf("invalid arg amount %d ,must be even", len(args))
-				}
-				var tokens []*token
-				for i := 0; i < len(args); i += 2 {
-					id, err := strconv.Atoi(args[i])
-					if err != nil {
-						log.Fatalf("can not parse arg %d : %s , %v", i, args[i], err)
-					}
-					address := args[i+1]
-					if !common.IsHexAddress(address) {
-						log.Fatalf("%s is not a valid address", address)
-					}
-					netCfg := conf.GetNetwork(uint64(id))
-					if netCfg == nil {
-						log.Fatalf("network with chainId %d not found in config file", id)
-					}
-					tokens = append(tokens, &token{uint64(id), address, netCfg})
-				}
-				for i := 0; i < len(tokens); i++ {
-					go func(i int) {
-						log.Infof("Rebinding for %s...",tokens[i].NetCfg.Name)
-						client, err := ethclient.Dial(tokens[i].NetCfg.Provider)
-						if err != nil {
-							err = fmt.Errorf("fail to dial %s , %s", tokens[i].NetCfg.Provider, err)
-							sig <- Msg{tokens[i].ChainId, err}
-							return
-						}
-						for j := 0; j < len(tokens); j++ {
-							if i == j {
-								continue
-							}
-							err = shutTools.BindToken(
-								multiple,
-								client,
-								tokens[i].NetCfg,
-								common.HexToAddress(tokens[i].Address),
-								tokens[j].ChainId,
-								common.FromHex(tokens[j].Address))
-							if err != nil {
-								err = fmt.Errorf(
-									"fail to shut bind from chain %d =>to=> chain %d , %s",
-									tokens[i].ChainId,
-									tokens[j].ChainId,
-									err)
-								sig <- Msg{tokens[i].ChainId, err}
-								return
-							}
-							log.Infof("%d =>to=> %d pair has be rebind",tokens[i].ChainId,tokens[j].ChainId)
-						}
-						sig <- Msg{tokens[i].ChainId, nil}
-					}(i)
-				}
-				cnt := len(tokens)
-				for msg := range sig {
-					cnt -= 1
-					if msg.Err != nil {
-						log.Error(msg.Err)
-					} else {
-						log.Infof("Token at chain %d has been rebind.", msg.ChainId)
-					}
-					if cnt == 0 {
-						log.Info("Done.")
-						break
-					}
-				}
-		*/
->>>>>>> Stashed changes
 	default:
 		log.Fatal("unknown function", function)
 	}
